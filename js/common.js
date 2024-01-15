45c39ef7a55ac20e4a48979d95af3c6f063a6adb
dlg <121958873+dlg1991@users.noreply.github.com>
2024-01-15 19:07:00 +0700

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,4 +1,20 @@
-
+ var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        margin: 10,
+        loop: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            // 600: {
+            //     items: 2
+            // },
+            // 1000: {
+            //     items: 3
+            // }
+        }
+    })
 // function isJson(item) {
 //     item = typeof item !== "string"
 //         ? JSON.stringify(item)
